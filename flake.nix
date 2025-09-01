e0cfd95089ac72d3d9bf2b016fbcb9fcf4b8ab5d
Tobias Brumhard <code@brumhard.com>
2025-09-01 07:26:41 +0200

    Use uv2nix to fix flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,22 @@
 {
-  # TODO: fix with https://medium.com/@daniel.garcia_57638/nix-nirvana-packaging-python-apps-with-uv2nix-c44e79ae4bc9
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -12,32 +24,59 @@
   outputs = {
     self,
     nixpkgs,
+    uv2nix,
     pyproject-nix,
+    pyproject-build-systems,
   }: let
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    project = pyproject-nix.lib.project.loadPyproject {
-      # Read & unmarshal pyproject.toml relative to this project root.
-      # projectRoot is also used to set `src` for renderers such as buildPythonPackage.
-      projectRoot = ./.;
-    };
   in {
     devShells = forAllSystems (system: let
       pkgs = import nixpkgs {inherit system;};
+      python = pkgs.python3;
     in {
       default = pkgs.mkShellNoCC {
         packages = with pkgs; [
+          python
           uv
         ];
+        env = {
+          UV_PYTHON_DOWNLOADS = "never";
+          UV_PYTHON = python.interpreter;
+        };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
       };
     });
 
     packages = forAllSystems (system: let
       pkgs = import nixpkgs {inherit system;};
-      python = pkgs.python3;
-      attrs = project.renderers.buildPythonPackage {inherit python;};
+      python = pkgs.python313;
+      workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+      pythonSet =
+        # Use base package set from pyproject.nix builders
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+        (
+          pkgs.lib.composeManyExtensions [
+            pyproject-build-systems.overlays.default
+            overlay
+          ]
+        );
     in {
-      default = python.pkgs.buildPythonPackage attrs;
+      default = pythonSet.mkVirtualEnv "scooter-env" workspace.deps.default;
+    });
+
+    apps = forAllSystems (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/scooter";
+      };
     });
   };
 }
